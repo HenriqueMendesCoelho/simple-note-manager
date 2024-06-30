dde0c8f6569a0057c69b6be36e39932ce3caf2df
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2024-06-30 16:16:43 -0300

    fix: mongoClient URI from env

--- src/app/mongoClient/MongoClient.ts
+++ src/app/mongoClient/MongoClient.ts
@@ -5,7 +5,7 @@ if (!MONGO_URI) {
   throw new Error('Please define the MONGO_URI environment variable');
 }
 
-const uri = 'mongodb://mongodb:y7EJC38qNd6V@localhost:27017/'; // Replace with your MongoDB URI
+const uri = MONGO_URI;
 const mongoClient = new MongoClient(uri);
 
 export { mongoClient };

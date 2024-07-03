ea140beb38b2cf13f8bb2804aaf029f1b16b71e8
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2024-07-02 21:01:34 -0300

    chore: added cors plugin and logging requests

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,17 @@ import fastify from 'fastify';
 import { mongoClient } from './app/mongoClient/MongoClient.js';
 import routes from './routes.js';
 import startup from './app/boot/index.js';
+import cors from '@fastify/cors';
 
 const server = fastify();
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 5000;
 server.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
   if (err) throw err;
   console.log(`Server listening on: ${address}`);
+});
+server.register(cors, {
+  origin: '*',
 });
 
 server.addHook('onResponse', (request, reply, done) => {

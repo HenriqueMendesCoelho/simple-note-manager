771fba2e8be482ced7dd6e1441b9cfdefc9f6232
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2024-09-01 21:05:40 -0300

    fix: login schema

--- src/authentication/routes.ts
+++ src/authentication/routes.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { authenticationController as controller } from './index.js';
+import LoginRequestSchema from './adapter/controller/schemas/LoginRequestSchema.js';
 
 async function authRoutes(fastify: FastifyInstance) {
-  fastify.post('/login', async (request: FastifyRequest, reply: FastifyReply) => {
+  fastify.post('/login', { schema: LoginRequestSchema }, async (request: FastifyRequest, reply: FastifyReply) => {
     const res = await controller.login(request, reply);
     reply.send(res);
   });
